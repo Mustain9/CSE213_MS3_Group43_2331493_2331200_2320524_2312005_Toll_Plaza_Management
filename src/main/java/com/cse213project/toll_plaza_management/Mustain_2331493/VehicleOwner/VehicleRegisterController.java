231d5c0bfa0daedb76f844ee36600a2af4af3413
Mustain9 <mustainbellah@gmail.com>
2025-08-09 00:10:35 +0600

    Project

--- src/main/java/com/cse213project/toll_plaza_management/Mustain_2331493/VehicleOwner/VehicleRegisterController.java
+++ src/main/java/com/cse213project/toll_plaza_management/Mustain_2331493/VehicleOwner/VehicleRegisterController.java
@@ -6,11 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
@@ -86,15 +82,56 @@ public class VehicleRegisterController {
 
     @FXML
     public void btnRegisterVehicle(ActionEvent actionEvent) {
-        Vehicle vehicleToBeAdded =  new Vehicle(
-                txtOwnerName.getText(),
-                txtEmail.getText(),
-                txtVehicleNumber.getText(),
-                comboVehicleType.getValue(),
-                Integer.parseInt(txtphoneNumber.getText()));
+
+        String ownerName = txtOwnerName.getText();
+        String email = txtEmail.getText();
+        String vehicleNumber = txtVehicleNumber.getText();
+        String vehicleType = comboVehicleType.getValue();
+        String phoneNumberText = txtphoneNumber.getText();
+
+
+
+        if (vehicleNumber.isEmpty() || vehicleType == null || phoneNumberText.isEmpty() ||
+                ownerName.isEmpty() || email.isEmpty()) {
+            showAlert("Input Error", "All fields are required.");
+            return;
+        }
+
+        int phoneNumber;
+
+        try {
+            phoneNumber = Integer.parseInt(phoneNumberText);
+        } catch (NumberFormatException e) {
+            showAlert("Invalid Input", "Phone Number must be a number.");
+            return;
+        }
+
+        Vehicle vehicleToBeAdded = new Vehicle(ownerName, email, vehicleNumber, vehicleType,phoneNumber);
 
         VehicleList.add(vehicleToBeAdded);
         tableRegisteredVehicles.getItems().add(vehicleToBeAdded);
+        clearFields();
+    }
+
+    private void showAlert(String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
+    private void clearFields() {
+        txtVehicleNumber.clear();
+        comboVehicleType.setValue(null);
+        txtphoneNumber.clear();
+        txtOwnerName.clear();
+        txtEmail.clear();}
+
+
+
+
+    @FXML
+    public void ClearField(ActionEvent actionEvent) {
 
     }
 }
