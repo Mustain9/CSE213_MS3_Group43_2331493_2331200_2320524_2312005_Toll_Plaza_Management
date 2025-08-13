d435531507deef8c2c49f253ca72bb9b35b5147b
sakim06 <neymarsakim@gmail.com>
2025-08-12 23:19:09 -0700

    New file with fxml and controller

---
+++ src/main/java/com/cse213project/toll_plaza_management/Sakim_2312005/Maintenance_Staff/UpdaterepairstatusController.java
@@ -0,0 +1,29 @@
+package com.cse213project.toll_plaza_management.Sakim_2312005.Maintenance_Staff;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class UpdaterepairstatusController
+{
+    @javafx.fxml.FXML
+    private ComboBox currentStatusCb;
+    @javafx.fxml.FXML
+    private TableColumn currentStatusTc;
+    @javafx.fxml.FXML
+    private TableView showIssueTv;
+    @javafx.fxml.FXML
+    private TextField issueIdTf;
+    @javafx.fxml.FXML
+    private TableColumn issueIdTc;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void repairUpdateOnaction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/cse213project/toll_plaza_management/Sakim_2312005/Maintenance_Staff/ViewactivemaintenceController.java
@@ -0,0 +1,34 @@
+package com.cse213project.toll_plaza_management.Sakim_2312005.Maintenance_Staff;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class ViewactivemaintenceController
+{
+    @javafx.fxml.FXML
+    private TextField locationTf;
+    @javafx.fxml.FXML
+    private TableColumn locationTc;
+    @javafx.fxml.FXML
+    private DatePicker dateReportedDp;
+    @javafx.fxml.FXML
+    private TextField issueIdTf;
+    @javafx.fxml.FXML
+    private TableColumn dateTc;
+    @javafx.fxml.FXML
+    private TableColumn issueIdTc;
+    @javafx.fxml.FXML
+    private ComboBox issueTypeCb;
+    @javafx.fxml.FXML
+    private TableColumn issueTypeTc;
+    @javafx.fxml.FXML
+    private TableView activeIssueTv;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void viewMaintenanceOnaction(ActionEvent actionEvent) {
+    }
+}
