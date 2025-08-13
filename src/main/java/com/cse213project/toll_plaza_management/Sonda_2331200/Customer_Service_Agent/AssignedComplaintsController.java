981a6dc2e92975b7b9f690980018ba4896893c60
SaimaSonda <saimasalamsonda@gmail.com>
2025-08-13 13:28:34 +0600

    Update

---
+++ src/main/java/com/cse213project/toll_plaza_management/Sonda_2331200/Customer_Service_Agent/AssignedComplaintsController.java
@@ -0,0 +1,10 @@
+package com.cse213project.toll_plaza_management.Sonda_2331200.Customer_Service_Agent;
+
+public class AssignedComplaintsController
+{
+    @javafx.fxml.FXML
+    private TableView assignedComplaintsTable;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
