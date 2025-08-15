23b570c5408cd39df1adad8a120ff94b3d34d027
keiz7en <103592242+keiz7en@users.noreply.github.com>
2025-08-15 18:11:58 +0600

    add accountant and managing director roles to login and update related fxml files and module dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.cse213project.cse213_ms3_group43_2331493_2331200_2320524_2312005_toll_plaza_management {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.apache.pdfbox;
+    requires org.apache.fontbox;
 
     opens com.cse213project.toll_plaza_management to javafx.fxml, javafx.graphics;
     opens com.cse213project.toll_plaza_management.common to javafx.fxml;
@@ -8,6 +10,10 @@ module com.cse213project.cse213_ms3_group43_2331493_2331200_2320524_2312005_toll
     opens com.cse213project.toll_plaza_management.Mustain_2331493.TollCollector to javafx.fxml;
     opens com.cse213project.toll_plaza_management.Sonda_2331200.Supervisor to javafx.fxml;
     opens com.cse213project.toll_plaza_management.Sonda_2331200.Customer_Service_Agent to javafx.fxml;
+    opens com.cse213project.toll_plaza_management.Auvi_2320524.Accountaant to javafx.fxml;
+    opens com.cse213project.toll_plaza_management.Auvi_2320524.Accountaant.model to javafx.fxml;
+    opens com.cse213project.toll_plaza_management.Auvi_2320524.ManagingDirector to javafx.fxml;
+    opens com.cse213project.toll_plaza_management.Auvi_2320524.ManagingDirector.model to javafx.fxml;
 
     exports com.cse213project.toll_plaza_management;
     exports com.cse213project.toll_plaza_management.common;
@@ -15,5 +21,9 @@ module com.cse213project.cse213_ms3_group43_2331493_2331200_2320524_2312005_toll
     exports com.cse213project.toll_plaza_management.Mustain_2331493.TollCollector;
     exports com.cse213project.toll_plaza_management.Sonda_2331200.Supervisor;
     exports com.cse213project.toll_plaza_management.Sonda_2331200.Customer_Service_Agent;
+    exports com.cse213project.toll_plaza_management.Auvi_2320524.Accountaant;
+    exports com.cse213project.toll_plaza_management.Auvi_2320524.Accountaant.model;
+    exports com.cse213project.toll_plaza_management.Auvi_2320524.ManagingDirector;
+    exports com.cse213project.toll_plaza_management.Auvi_2320524.ManagingDirector.model;
 
 }
